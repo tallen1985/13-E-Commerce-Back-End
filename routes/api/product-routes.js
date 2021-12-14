b34fff12da8ec329aab3c1ef91d9e25f1ed4d976
Troy Allen <jake.allen@me.com>
2021-12-14 11:28:21 -0500

    updated product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -141,9 +141,9 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json("Successfully deleted");
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json('Internal Server Error, please try later');
   }
 });
 
